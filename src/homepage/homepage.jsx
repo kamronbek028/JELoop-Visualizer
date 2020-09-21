807527498c0f6b9ade6d559047c6aac8f18730cf
Kamronbek <kamronbek028@gmail.com>
2020-09-21 23:04:57 +0500

    adding starting files

---
+++ src/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+class HomePage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="homepage">homepage</div>;
+  }
+}
+
+export default HomePage;
